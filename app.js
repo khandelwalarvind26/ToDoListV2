eb888330cdacdfad65f7a2f0a21d8d06530c0e4c
Arvind <khandelwalarvind26@gmail.com>
2022-07-26 20:40:15 +0530

    Added CRUD database functionality for single list

--- app.js
+++ app.js
@@ -3,20 +3,53 @@ const BS = require('body-parser');
 const date = require(__dirname + "/date.js");
 const https = require('https');
 const app = express();
+const mongoose = require('mongoose');
 app.use(BS.urlencoded({extended : true}));
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-let tasks = ["Cook Food", "Eat Food", "Wash Utensils"];
-let workTasks = [];
-app.get('/',function(req,res) {
-    let day = date.getDate();
-    res.render('list',{title:day,task:tasks});
+mongoose.connect("mongodb://localhost:27017/"+"todolistDB");
+const taskSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    }
 });
 
-app.get("/work",function(req,res) {
-    res.render("list",{title:"Work List",task:workTasks});
+const Task = mongoose.model('Task',taskSchema); 
+
+function buildDB(curr) {
+    const item1 = new curr({
+        name:"Welcome To your ToDo List!"
+    });
+
+    const item2 = new curr({
+        name:"Hit the + button to add new tasks."
+    });
+
+    const item3 = new curr({
+        name:"<-- Hit this checkbox to delete a task."
+    });
+    const itemList = [item1,item2,item3];
+    curr.insertMany(itemList,function(err) {
+        if(err) console.log(err);
+        else console.log("Success");
+    });
+    return itemList;
+}
+
+
+app.get('/',function(req,res) {
+    let day = date.getDate();
+    Task.find({},function(err,task){
+        if(err) console.log(err);
+        else {
+            if(task.length == 0) task = buildDB(Task);
+        }
+        res.render('list',{title:day,task:task});
+    });
+    // console.log(tasks);
 });
 
 app.get("/about",function(req,res) {
@@ -25,16 +58,26 @@ app.get("/about",function(req,res) {
 
 app.post("/",function(req,res){
     let task = req.body.newtask;
-    console.log(task);
-    tasks.push(task);
+    // console.log(task);
+    const item = new Task({
+        name:task
+    });
+    item.save();
     res.redirect("/");
 });
 
-app.post("/work",function(req,res) {
-    let task = req.body.newtask;
-    workTasks.push(task);
-    res.redirect("/work");
+app.post("/delete",function(req,res) {
+    const id = req.body.checked;
+    // console.log(req.url);
+    Task.deleteOne({_id:id},function(err) {
+        if(err) console.log(err);
+        else console.log("Deleted");
+    });
+    // console.log(req.body.h1);
+    res.redirect("/");
 });
+
+
 
 app.listen(3000,function() {
     console.log("Server started on port 3000");
